d206487bbe21047d63401913688805d1abb42d62
CorneilleEdi <gallasedi@gmail.com>
2019-04-20 05:59:45 +0530

    url

--- headerparser/server.js
+++ headerparser/server.js
@@ -1,35 +1,136 @@
-// init project
+'use strict';
+
 var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var dns = require("dns");
+var bodyParser = require("body-parser");
+
+var cors = require('cors');
+
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+// Basic Configuration 
+var port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MONGO_URI);
+
+var urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
 });
 
+var Url = mongoose.model("Url", urlSchema);
 
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/whoami",function (req, res) {
-  res.json({"ipaddress":req.ip,"language":req.headers["accept-language"],"software":req.headers["user-agent"]});
+
+app.post("/api/shorturl/new", function(req, res) {
+  console.log(req.body)
+  var url = req.body.url;
+  var dnsLookup = new Promise(function(resolve, reject) {
+    var result = url.replace(/(^\w+:|^)\/\//, "");
+    dns.lookup(result, function(err, addresses, family) {
+      if (err) reject(err);
+      resolve(addresses);
+    });
+  });
+
+  dnsLookup
+    .then(function() {
+      return checkIfExists(url);
+    })
+    .then(function(data) {
+      if (data.status) {
+        return res.json({ original_url: url, short_url: data.short_url });
+      } else {
+        var shortUrl = shorterUrl();
+        var urlMapping = new Url({
+          original_url: url,
+          short_url: shortUrl
+        });
+        return saveUrlMapping(urlMapping);
+      }
+    })
+    .then(function(original_url) {
+      var shortUrl = shorterUrl();
+      return res.json({ original_url: url, short_url: shortUrl });
+    });
+  dnsLookup.catch(function(reason) {
+    return res.json({ error: "invalid URL" });
+  });
 });
 
-// {"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5",
-// "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+app.get("/api/shorturl/:shortUrl", function(req, res) {
+  var redirectPromise = redirectToOriginalUrl(req.params.shortUrl);
+  redirectPromise.then(function(original_url) {
+    return res.redirect(original_url);
+  });
+  redirectPromise.catch(function(reason) {
+    return res.json({ error: "invalid URL" });
+  });
+});
+
+function redirectToOriginalUrl(short_url) {
+  return new Promise(function(resolve, reject) {
+    Url.findOne({ short_url: short_url }, function(err, doc) {
+      if (err || doc === null) return reject(err);
+      else return resolve(doc.original_url);
+    });
+  });
+}
+
+function checkIfExists(original_url) {
+  return new Promise(function(resolve, reject) {
+    Url.findOne({ original_url: original_url }, function(err, doc) {
+      if (doc === null || err) resolve({ status: false });
+      else resolve({ status: true, short_url: doc.short_url });
+    });
+  });
+}
+
+function saveUrlMapping(mapping) {
+  return new Promise(function(resolve, reject) {
+    mapping.save(function(err, data) {
+      if (err) return reject(err);
+      else return resolve(null, data);
+    });
+  });
+}
+
+function shorterUrl() {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
